db98fa268b55e9a7003068c227a1f638fc1ef896
Master_iNeuron <127201746+MasteriNeuron@users.noreply.github.com>
2023-03-23 16:46:00 +0530

    Update application.py

--- application.py
+++ application.py
@@ -111,5 +111,5 @@ def index():
 
 if __name__ == "__main__":
     #app.run(debug=False,host='0.0.0.0')
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
 	#app.run(debug=True)
