90ecd2d83f5a69897893d6a7b726e79d8208a42d
Master_iNeuron <127201746+MasteriNeuron@users.noreply.github.com>
2023-05-25 16:48:53 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import pymongo
+import csv
 
 application = Flask(__name__) # initializing a flask app
 app=application
@@ -31,48 +32,45 @@ def index():
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
             prod_html = bs(prodRes.text, "html.parser")
-            print(prod_html)
             commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
 
             filename = searchString + ".csv"
-            fw = open(filename, "w")
-            headers = "Product, Customer Name, Rating, Heading, Comment \n"
-            fw.write(headers)
-            reviews = []
-            for commentbox in commentboxes:
-                try:
-                    #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+            with open(filename, "w", newline='', encoding='utf-8') as fw:
+                headers = ["Product", "Customer Name", "Rating", "Heading", "Comment"]
+                writer = csv.DictWriter(fw, fieldnames=headers)
+                writer.writeheader()
 
-                except:
-                    name = 'No Name'
+                reviews = []
+                for commentbox in commentboxes:
+                    try:
+                        name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+                    except:
+                        name = 'No Name'
 
-                try:
-                    #rating.encode(encoding='utf-8')
-                    rating = commentbox.div.div.div.div.text
+                    try:
+                        rating = commentbox.div.div.div.div.text
+                    except:
+                        rating = 'No Rating'
 
+                    try:
+                        commentHead = commentbox.div.div.div.p.text
+                    except:
+                        commentHead = 'No Comment Heading'
 
-                except:
-                    rating = 'No Rating'
+                    try:
+                        comtag = commentbox.div.div.find_all('div', {'class': ''})
+                        custComment = comtag[0].div.text
+                    except Exception as e:
+                        print("Exception while creating dictionary: ", e)
 
-                try:
-                    #commentHead.encode(encoding='utf-8')
-                    commentHead = commentbox.div.div.div.p.text
+                    mydict = {"Product": searchString, "Customer Name": name, "Rating": rating, "Heading": commentHead, "Comment": custComment}
+                    reviews.append(mydict)
 
-                except:
-                    commentHead = 'No Comment Heading'
-                try:
-                    comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
-                    custComment = comtag[0].div.text
-                except Exception as e:
-                    print("Exception while creating dictionary: ",e)
+                writer.writerows(reviews)
 
-                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
-                          "Comment": custComment}
-                reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
-            db = client['review_scrap']
+               
+            client = pymongo.MongoClient("mongodb+srv://Alonewinner:Alonewinner@cluster1.x8c5jdd.mongodb.net/?retryWrites=true&w=majority")
+            db = client['flipkart_scrap']
             review_col = db['review_scrap_data']
             review_col.insert_many(reviews)
             return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
